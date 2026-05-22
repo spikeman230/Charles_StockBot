310e49b853a32bdf312dab08b8e8aeb638476188
spikeman230 <charles5529@gmail.com>
2026-05-22 12:05:10 +0800

    Update noc_core.py

--- noc_core.py
+++ noc_core.py
@@ -170,7 +170,7 @@ class NOCStrategy:
             if current > ma10 and ma10 > ma20:
                 return 1.0
             else:
-            return -1.0
+                return -1.0
         else:
             # 🐻 重裝防禦模式：維持嚴格季線邏輯
             ma60 = hist_df['Close'].rolling(60).mean()
